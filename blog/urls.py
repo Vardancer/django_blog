33236021e11f87a09bca777db39f2213ba6320ad
vardancer <vardancer@gmail.com>
2019-03-22 17:18:27 +0300

    1

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from blog.views import ArticleListView, ArticleDetailView
 
 urlpatterns = [
     path('', ArticleListView.as_view(), name='article-list'),
-    path('article/<int:id>/', ArticleDetailView.as_view(), name='article'),
+    path('article/<int:pk>/', ArticleDetailView.as_view(), name='article'),
 ]
 
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, CreateView
 from blog.models import Article, Comment
+from blog.forms import AddCommentForm
 
 # Create your views here.
 
@@ -14,9 +15,16 @@ class ArticleListView(ListView):
 class ArticleDetailView(DetailView):
     model = Article
     template_name = 'article-detail.html'
+    context_object_name = 'article'
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['comments'] = Comment.objects.filter(article=id)
+        context['comments'] = Comment.objects.filter(article=self.kwargs['pk'])
+
+        return context
+
+
+class AddComment(CreateView):
+    form_class = AddCommentForm
 
 
