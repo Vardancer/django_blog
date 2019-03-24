145715099d935dac53379bebe320561fd98831b2
trol <vardancer@live.ru>
2019-03-24 10:38:20 +0300

    0

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'debug_toolbar',
+    'bootstrap4',
+    'ckeditor',
     'blog',
 ]
 
@@ -122,3 +124,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
