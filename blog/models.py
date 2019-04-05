360c6f4cd3d8fa8fcc360f2333b4ee5174bd0432
trol <vardancer@live.ru>
2019-04-05 22:33:28 +0300

    0

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 
@@ -24,4 +25,43 @@ class Comment(models.Model):
 
     def __str__(self):
         return "{} -- {}".format(self.article.title, self.date_add)
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+
+
+class Survey(models.Model):
+    description = models.CharField(max_length=150)
+    is_published = models.BooleanField(default=False)
+    need_logged_user = models.BooleanField(default=True)
+
+    def __str__(self):
+        return self.description
+
+    def get_absolute_url(self):
+        return reverse("survey-detail", kwargs={"id": self.pk})
+
+
+class Questions(models.Model):
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.text
+
+
+class Answers(models.Model):
+    question = models.ForeignKey(Questions, on_delete=models.CASCADE)
+    survey = models.ForeignKey(Survey, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+    is_checked = models.BooleanField(default=False)
+
+    class Meta:
+        unique_together = ("question", "survey")
+
+    def __str__(self):
+        return self.question
+
+
 
